df5d3ee4dcf034fd4f583109384ca3a465da8017
Euan Williams <fromeuan@gmail.com>
2020-02-06 23:38:58 -0500

    Steno Models
    Wrote models for steno combinations, collections, and categories and registered them in admin

--- briefology/lookup/admin.py
+++ briefology/lookup/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Collection, Combination, Steno
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Collection)
+admin.site.register(Combination)
+admin.site.register(Steno)
--- briefology/lookup/models.py
+++ briefology/lookup/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=50)
+
+class Collection(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, related_name='collections', on_delete=models.CASCADE, blank=True)
+    created_by = models.ForeignKey(User, related_name='collections')
+
+class Combination(models.Model):
+    english = models.CharField(max_length=30)
+    steno = models.ManyToManyField(Steno, related_name='combinations')
+    collection = models.ForeignKey(Collection, related_name='combinations', on_delete=models.CASCADE, blank=True)
+    created_by = models.ForeignKey(User, related_name='combinations')
+
+class Steno(models.Model):
+    steno = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['steno']
+
+    def __str__(self):
+        return self.steno
