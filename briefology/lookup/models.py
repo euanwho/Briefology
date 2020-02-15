a6ab283f1fe9a6612d57c04f57ad256a5cdd00f6
Euan Williams <fromeuan@gmail.com>
2020-02-14 23:16:33 -0500

    Added BriefList in models.py
    Added BriefList and made Steno have a many-to-many relationship with it

--- briefology/lookup/models.py
+++ briefology/lookup/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+PUB_PRIV = {
+    ('P', 'Public'),
+    ('R', 'Private')
+}
+
+class BriefList(models.Model):
+    name = models.CharField(max_length=30, default='')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, default='P', choices=PUB_PRIV)
+
+    def __str__(self):
+        return self.status
+
 class Dictionary(models.Model):
-    name = models.CharField(max_length=20)
-    date_posted = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=30)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, default='P', choices=PUB_PRIV)
 
     def __str__(self):
         return self.name
@@ -11,13 +27,17 @@ class Steno(models.Model):
     steno = models.CharField(max_length=50)
     word = models.ForeignKey('Word', on_delete=models.CASCADE, related_name='combinations', default='', db_index=True)
     source = models.ForeignKey(Dictionary, on_delete=models.SET_NULL, null=True, related_name='entries')
+    brief_list = models.ManyToManyField(BriefList)
+    dictionary = models.ManyToManyField(Dictionary)
 
     class Meta():
-        ## add logic for if there is a slash in steno and if not?
         ordering = ['steno']
-
+    
     def __str__(self):
         return self.steno
+
+    def __len__(self):
+        return len(self.steno)
 
 class Word(models.Model):
     word = models.CharField(max_length=50, unique=True)
